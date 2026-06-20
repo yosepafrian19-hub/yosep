4c5ac580c7057e4b3cd21072175bea33b427d82b
yosepafrian19-hub <yosepafrian19@gmail.com>
2026-06-21 02:06:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+const http = require("http");
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("SERVER HIDUP 🚀");
+});
+
+server.listen(port, () => {
+  console.log("Server jalan di port " + port);
+});
